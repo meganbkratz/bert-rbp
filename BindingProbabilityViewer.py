9bcf007a6580a19ed37d7e407d6d79440e7415b1
Megan Kratz <meganbkratz@gmail.com>
2022-06-23 16:38:04 -0400

    fixes for plotting

--- BindingProbabilityViewer.py
+++ BindingProbabilityViewer.py
@@ -1,25 +1,26 @@
+import argparse
 import pyqtgraph as pg 
-from plotting_helpers import load_probabilities,
+from plotting_helpers import load_probabilities
 
 
 class BindingProbabilityViewer(pg.QtGui.QWidget):
 
 	def __init__(self, probability_file):
-		pg.QtGui.QWidget.__init__(self)
+		pg.QtWidgets.QWidget.__init__(self)
 
-		self.layout = pg.QtGui.QGridLayout()
+		self.layout = pg.QtWidgets.QGridLayout()
 		self.layout.setContentsMargins(3,3,3,3)
 		self.setLayout(self.layout)
 
-		self.label = pg.QtGui.QLabel('File: %s' %probability_file)
+		self.label = pg.QtWidgets.QLabel('File: %s' %probability_file)
 		self.genomePlot = pg.PlotWidget(labels={'left':'binding probability', 'bottom':'DNA nucleotide number'}, title='Genome-Aligned probabilities')
 		self.genomePlot.addLegend()
 		self.rnaPlot = pg.PlotWidget(labels={'left':'binding probability', 'bottom':'RNA nucleotide number'}, title='RNA-aligned probabilities')
 		self.rnaPlot.addLegend()
 
-		self.layout.addItem(self.label)
-		self.layout.addItem(self.genomePlot)
-		self.layout.addItem(self.rnaPlot)
+		self.layout.addWidget(self.label, 0,0)
+		self.layout.addWidget(self.genomePlot, 1,0)
+		self.layout.addWidget(self.rnaPlot, 2,0)
 
 		self.newFileSelected(probability_file)
 
@@ -27,7 +28,7 @@ class BindingProbabilityViewer(pg.QtGui.QWidget):
 		self.probs = load_probabilities(probability_file)
 		self.plot_probabilities()
 
-	def plot_probabilities(self)
+	def plot_probabilities(self):
 		probs = self.probs
 
 		self.genomePlot.clear()
@@ -38,8 +39,8 @@ class BindingProbabilityViewer(pg.QtGui.QWidget):
 			if k in ['indices', 'genomic_indices']:
 				continue
 			if probs['indices'].get('dna_indices') is not None:
-				self.genomePlot.plot(x=probs['indices']['dna_indices'][k], y=probs[k], symbolBrush=pg.mkPen(pens[i]), name=k, pen=None, symbolPen=None)
-			self.rnaPlot.plot(x=probs['indices']['rna_indices'][k], y=probs[k], symbolBrush=pg.mkPen(pens[i]), name=k, pen=None, symbolPen=None)
+				self.genomePlot.plot(x=probs['indices']['dna_indices'][k], y=probs[k], symbolBrush=pg.mkColor(pens[i]), name=k, pen=None, symbolPen=None)
+			self.rnaPlot.plot(x=probs['indices']['rna_indices'][k], y=probs[k], symbolBrush=pg.mkColor(pens[i]), name=k, pen=None, symbolPen=None)
 
 		
 if __name__ == '__main__':
@@ -47,14 +48,11 @@ if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser()
 
-	parser.addArgument('probability_file', type=str, help="The path to the probability file to load.")
+	parser.add_argument('probability_file', type=str, help="The path to the probability file to load.")
 
 	args = parser.parse_args()
 
 	mw = BindingProbabilityViewer(args.probability_file)
-
-
-
-
+	mw.show()
 
 
