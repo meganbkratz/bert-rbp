95d0cc6f2947156015e68ed22ba8006f56dde9fc
Megan Kratz <meganbkratz@gmail.com>
2022-12-06 16:00:23 -0500

    modified script to look at performance of training vs eval data

--- create_roc_files.py
+++ create_roc_files.py
@@ -7,34 +7,38 @@ dataset_path = '/proj/magnuslb/users/mkratz/bert-rbp/datasets'
 
 RBP = sys.argv[1]
 
-p = os.path.join(dataset_path, RBP, 'nontraining_sample_finetune', 'dev_%s_probabilities.pk'%RBP)
-probs=load_probabilities(p)
+p_eval = os.path.join(dataset_path, RBP, 'nontraining_sample_finetune', 'dev_%s_bertrbp_output.pk'%RBP)
+p_train = os.path.join(dataset_path, RBP, 'training_sample_finetune', 'train_%s_bertrbp_output.pk'%RBP)
 
-pos = probs['positives']
-neg = probs['negatives']
 
-data = np.zeros(100, dtype=[
-	('threshold', float),
-	('true_positives', int),
-	('false_positives', int),
-	('true_negatives',int),
-	('false_negatives',int),
-	('pos_hist', int),
-	('neg_hist', int)
-	])
+for k, v in {'eval': p_eval, 'train':p_train}.items():
+	probs=load_probabilities(v)
 
-pos_hist, bins = np.histogram(pos, bins=100, range=(0,1))
-neg_hist, bins = np.histogram(neg, bins=100, range=(0,1))
+	pos = probs['positives']
+	neg = probs['negatives']
 
-for i,t in enumerate(np.arange(0,1,0.01)):
-	data[i]['threshold'] = t
-	data[i]['true_positives'] = len(pos[pos >= t])
-	data[i]['false_positives'] = len(neg[neg >= t])
-	data[i]['true_negatives'] = len(neg[neg < t])
-	data[i]['false_negatives'] = len(pos[pos < t])
-	data[i]['pos_hist'] = pos_hist[i]
-	data[i]['neg_hist'] = neg_hist[i]
+	data = np.zeros(100, dtype=[
+		('threshold', float),
+		('true_positives', int),
+		('false_positives', int),
+		('true_negatives',int),
+		('false_negatives',int),
+		('pos_hist', int),
+		('neg_hist', int)
+		])
 
-header = str(data.dtype.names).strip('(').strip(')').strip()
-filename = '/proj/magnuslb/users/mkratz/data/RBP_performance/%s_performance.csv'%RBP
-np.savetxt(filename, data, delimiter=',', header=header, fmt=['%.2f']+['%i']*6, comments='')
+	pos_hist, bins = np.histogram(pos, bins=100, range=(0,1))
+	neg_hist, bins = np.histogram(neg, bins=100, range=(0,1))
+
+	for i,t in enumerate(np.arange(0,1,0.01)):
+		data[i]['threshold'] = t
+		data[i]['true_positives'] = len(pos[pos >= t])
+		data[i]['false_positives'] = len(neg[neg >= t])
+		data[i]['true_negatives'] = len(neg[neg < t])
+		data[i]['false_negatives'] = len(pos[pos < t])
+		data[i]['pos_hist'] = pos_hist[i]
+		data[i]['neg_hist'] = neg_hist[i]
+
+	header = str(data.dtype.names).strip('(').strip(')').strip()
+	filename = '/proj/magnuslb/users/mkratz/data/RBP_performance/%s_%s_performance.csv'%(RBP, k)
+	np.savetxt(filename, data, delimiter=',', header=header, fmt=['%.2f']+['%i']*6, comments='')
