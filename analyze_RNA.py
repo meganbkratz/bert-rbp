0a870cd1eec868040750b1ce0d0aad2af15bb44f
Megan Kratz <meganbkratz@gmail.com>
2023-04-25 20:28:35 -0400

    linting fixes

--- analyze_RNA.py
+++ analyze_RNA.py
@@ -1,20 +1,17 @@
-import os, argparse
+import os
+import argparse
+from collections import OrderedDict
+import numpy as np
 import torch
+from torch.utils.data import DataLoader
 from Bio import SeqIO
-from src.transformers_DNABERT import BertConfig, BertForSequenceClassification, DNATokenizer
+from src.transformers_DNABERT import BertForSequenceClassification, DNATokenizer
 from src.transformers_DNABERT.data.processors.utils import InputExample, DataProcessor
 from src.transformers_DNABERT import glue_convert_examples_to_features as convert_examples_to_features
 from motif.motif_utils import seq2kmer
-from torch.utils.data import TensorDataset, DataLoader
-from collections import OrderedDict
 from util import parse_dna_range
-import numpy as np
 import config
-try:
-    import pyqtgraph as pg
-    HAVE_PYQTGRAPH = True
-except ImportError:
-    HAVE_PYQTGRAPH = False
+
 
 MAX_LENGTH = 101
 
@@ -24,14 +21,14 @@ def load_fasta_sequences(f, tokenizer, n_kmer):
     global MAX_LENGTH
 
     datasets = OrderedDict()
-    dataset_indices = {'rna_indices':{}}
+    dataset_indices = {'rna_indices': {}}
     for splice in SeqIO.parse(f, 'fasta'):
-        examples=[]
-        indices=[]
+        examples = []
+        indices = []
         i = 0
         while i <= len(splice.seq)-MAX_LENGTH:
             kmer_sequence = seq2kmer(str(splice.seq[i:i+MAX_LENGTH].upper()), n_kmer).replace('U', 'T')
-            examples.append(InputExample(splice.id+'_%i'%i, text_a=kmer_sequence, label='0')) 
+            examples.append(InputExample(splice.id+'_%i' % i, text_a=kmer_sequence, label='0'))
             indices.append(i+MAX_LENGTH/2)
             i += 10
 
@@ -52,11 +49,12 @@ def load_fasta_sequences(f, tokenizer, n_kmer):
     datasets['indices'] = dataset_indices
     return datasets
 
+
 def load_fasta_genome(filename, tokenizer, n_kmer):
     global MAX_LENGTH
 
     datasets = OrderedDict()
-    dataset_indices = {'dna_indices':{}, 'rna_indices':{}, 'metainfo':{}}
+    dataset_indices = {'dna_indices': {}, 'rna_indices': {}, 'metainfo': {}}
     for splice in SeqIO.parse(filename, 'fasta'):
         examples = []
         dna_indices = []
@@ -68,7 +66,7 @@ def load_fasta_genome(filename, tokenizer, n_kmer):
         i = 0
         while i <= len(rna)-MAX_LENGTH:
             kmer_sequence = seq2kmer(str(rna[i:i+MAX_LENGTH]), n_kmer).replace('U', 'T')
-            examples.append(InputExample(splice.id+'_%i'%i, text_a=kmer_sequence, label='0'))
+            examples.append(InputExample(splice.id+'_%i' % i, text_a=kmer_sequence, label='0'))
             dna_indices.append(indices[int(i+MAX_LENGTH/2)][0])
             rna_indices.append(int(i+MAX_LENGTH/2))
             i += 10
@@ -86,7 +84,12 @@ def load_fasta_genome(filename, tokenizer, n_kmer):
         chromosome, start, end = parse_dna_range(splice.description)
 
         datasets[splice.id] = all_input_ids
-        dataset_indices['metainfo'][splice.id] = {'desc':splice.description, 'chromosome':chromosome, 'range_start':start, 'range_end':end}
+        dataset_indices['metainfo'][splice.id] = {
+            'desc': splice.description,
+            'chromosome': chromosome,
+            'range_start': start,
+            'range_end': end,
+            }
         dataset_indices['dna_indices'][splice.id] = dna_indices
         dataset_indices['rna_indices'][splice.id] = rna_indices
 
@@ -100,14 +103,15 @@ def load_tsv_sequences(filename, tokenizer):
 
     global MAX_LENGTH
 
-    data = DataProcessor._read_tsv(filename) ## produces a list of ['kmer sequence', 'label']
-    negatives=[]
-    positives=[]
+    data = DataProcessor._read_tsv(filename)  # produces a list of ['kmer sequence', 'label']
+    negatives = []
+    positives = []
     for i, entry in enumerate(data):
+        # in both cases label needs to be filled in, but we don't use it
         if entry[1] == '0':
-            negatives.append(InputExample('uid_%i'%i, text_a=entry[0], label='0')) ## label needs to be filled in, but we don't use it
+            negatives.append(InputExample('uid_%i' % i, text_a=entry[0], label='0'))
         elif entry[1] == '1':
-            positives.append(InputExample('uid_%i'%i, text_a=entry[0], label='0'))
+            positives.append(InputExample('uid_%i' % i, text_a=entry[0], label='0'))
 
     neg_features = convert_examples_to_features(
         negatives,
@@ -130,7 +134,7 @@ def load_tsv_sequences(filename, tokenizer):
     neg_input_ids = torch.tensor([f.input_ids for f in neg_features], dtype=torch.long)
     pos_input_ids = torch.tensor([f.input_ids for f in pos_features], dtype=torch.long)
 
-    return {'positives':pos_input_ids, 'negatives':neg_input_ids}
+    return {'positives': pos_input_ids, 'negatives': neg_input_ids}
 
 
 def predict(dataset, model_path):
@@ -154,15 +158,14 @@ def predict(dataset, model_path):
         if name in ['indices']:
             continue
         dataloader = DataLoader(data, batch_size=32, shuffle=False)
-        predictions=None
+        predictions = None
 
         for batch in dataloader:
             batch = batch.to(device)
 
             with torch.no_grad():
                 outputs = model(input_ids=batch)
-                #_, logits = outputs[:2] ## see modeling_bert.py line 390 for description of outputs -- right now we only get (and need) logits
-                logits = outputs[0]
+                logits = outputs[0]  # because we don't supply labels, we only get logits (not loss)
 
             preds = logits.detach().cpu().numpy()
             if predictions is None:
@@ -172,20 +175,22 @@ def predict(dataset, model_path):
 
         probs = softmax(torch.tensor(predictions, dtype=torch.float32)).numpy()
 
-        results[name] = probs[:,1]
+        results[name] = probs[:, 1]
 
-    results['indices'] = dataset.get('indices') ## just pass these through here
+    results['indices'] = dataset.get('indices')  # just pass these through here
     return results
 
+
 def save_probabilities(probs, file_name):
-    print("Saving model output in %s"%file_name)
+    print("Saving model output in %s" % file_name)
     import pickle
     with open(file_name, 'wb') as f:
         pickle.dump(probs, f)
 
+
 def load_probabilities(file_name, quiet=False):
     if not quiet:
-        print("Loading model output from %s" %file_name)
+        print("Loading model output from %s" % file_name)
     import pickle
     with open(file_name, 'rb') as f:
         probs = pickle.load(f)
@@ -196,45 +201,46 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument("RBP", type=str, help="The name of the RNA binding protien (RBP) to use.")
-    parser.add_argument("--sequence_path", default=None, type=str, required=False, help="(optional) The path to the sequence file to use. If not specified, the non-training data for the RBP will be used")
+    parser.add_argument("--sequence_path", default=None, type=str, required=False, help="(optional) The path to \
+        the sequence file to use. If not specified, the non-training data for the RBP will be used")
     parser.add_argument("--model_path", default=None, type=str, required=True, help="The path to the model to use")
     parser.add_argument("--save_path", default=None, type=str, required=True, help="Where to save the output data.")
     parser.add_argument("--kmer", type=int, default=3)
 
     args = parser.parse_args()
 
-    ### make sure we have a trained model
+    # make sure we have a trained model
     model_path = args.model_path
     if not os.path.exists(model_path):
         raise Exception('Could not find model at "%s".' % model_path)
 
-
-    ## find and load our sequence data
+    # find and load our sequence data
     if args.sequence_path is None:
-        sequence_path = os.path.join(dataset_path, args.RBP, 'nontraining_sample_finetune', 'dev.tsv')
+        sequence_path = os.path.join(config.dataset_path, args.RBP, 'nontraining_sample_finetune', 'dev.tsv')
     else:
         sequence_path = args.sequence_path
 
     if not os.path.exists(sequence_path):
-        raise Exception('Could not find sequence data at "%s". Path does not exist.' %sequence_path)
+        raise Exception('Could not find sequence data at "%s". Path does not exist.' % sequence_path)
 
-    tokenizer=DNATokenizer.from_pretrained(model_path) ## need the tokenizer to load the sequences
+    tokenizer = DNATokenizer.from_pretrained(model_path)  # need the tokenizer to load the sequences
 
     if sequence_path[-3:] == '.fa' or sequence_path[-6:] == '.fasta':
         try:
-            dataset= load_fasta_genome(sequence_path, tokenizer, args.kmer)
-            print("Loaded genomic sequence data from %s" %sequence_path)
+            dataset = load_fasta_genome(sequence_path, tokenizer, args.kmer)
+            print("Loaded genomic sequence data from %s" % sequence_path)
         except:
-            print("Loading RNA sequence data from fasta file: %s"%sequence_path)
+            print("Loading RNA sequence data from fasta file: %s" % sequence_path)
             dataset = load_fasta_sequences(sequence_path, tokenizer, args.kmer)
     elif sequence_path[-4:] == '.tsv':
-        print("Loading RNA sequence data from .tsv file: %s"%sequence_path)
+        print("Loading RNA sequence data from .tsv file: %s" % sequence_path)
         dataset = load_tsv_sequences(sequence_path, tokenizer)
     else:
-        raise Exception("Not sure how to load sequence from '%s'. It doesn't seem to be a .fa, .fasta, or .tsv file" % sequence_path)
-    print("Running probability predictions against model at %s ....."%model_path)
+        raise Exception("Not sure how to load sequence from '%s'. It doesn't seem to be \
+        a .fa, .fasta, or .tsv file" % sequence_path)
+    print("Running probability predictions against model at %s ....." % model_path)
     probs = predict(dataset, model_path)
-    probs['metainfo'] = {'model_path': model_path, 'sequence_path':sequence_path}
+    probs['metainfo'] = {'model_path': model_path, 'sequence_path': sequence_path}
 
     save_file = args.save_path
     save_probabilities(probs, save_file)
